5a1aa2d00d213a7b2d9273d659e47c3398e887b7
pawelwarzecha <pawelwarzecha99@gmail.com>
2024-09-16 11:09:29 +0200

    finished

--- dataDriven/src/main/java/dataDriven/dataProvide.java
+++ dataDriven/src/main/java/dataDriven/dataProvide.java
@@ -1,5 +1,7 @@
 package dataDriven;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -7,11 +9,11 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class dataProvide {
 
+    DataFormatter formatter = new DataFormatter();
 
     @Test(dataProvider = "driveTest")
     public void testCaseData(String greeting, String communication, String id){
@@ -23,14 +25,23 @@ public class dataProvide {
     @DataProvider(name = "driveTest")
     public Object[][] getData() throws IOException {
 
-        //Object[][] data = {{"a","b","1"},{"z","x","2"},{"q","w","3"}};
-        //return data;
-
-        FileInputStream fis = new FileInputStream("");
+        FileInputStream fis = new FileInputStream("C:\\Users\\HP Compat\\Desktop\\Java\\Selenium\\dataDriven\\sampleData.xlsx");
         XSSFWorkbook wb = new XSSFWorkbook(fis);
         XSSFSheet sheet = wb.getSheetAt(0);
         int rowcount = sheet.getPhysicalNumberOfRows();
-        XSSFRow rows = sheet.getRow(0);
-    }
+        XSSFRow row = sheet.getRow(0);
+        int colCount = row.getLastCellNum();
+        Object[][] data = new Object[rowcount - 1][colCount];
 
+        for (int i = 0; i < rowcount-1; i++) {
+
+            row = sheet.getRow(i+1);
+            for (int j = 0; j < colCount; j++) {
+
+                XSSFCell cell = row.getCell(j);
+                data[i][j] = formatter.formatCellValue(cell);
+            }
+        }
+        return data;
+    }
 }
